3166547cac1a763da2f036fcd87a55850a322e57
fatihserhatturan <95492309+fatihserhatturan@users.noreply.github.com>
2023-08-08 17:24:01 +0300

    update and getall

--- src/Controllers/todoController.js
+++ src/Controllers/todoController.js
@@ -32,10 +32,53 @@ const todoAdd= async(req,res)=>{
         message:"Sunuucu Hatasi!"
     })
     }
-    
+
+}
+const todoGetAll = async(req,res)=>{
+try {
+    const todoGetAll= await  todo.find({})
+    return res.status(200).json({
+        success:true,
+        data:todoGetAll
+    })
+} catch (error) {
+    return res.status(500).json({
+        success:false,
+        message:"Kayit Getirme İslemi Basarisiz"
+    })
+
 }
 
+}
+
+const todoUpdate = async(req,res)=>{
+    const{id}=req.params
+    try {
+        const todoUpdate=await todo.findByIdAndUpdate(id,req.body)
+        if(todoUpdate){
+            return res.status(200).json({
+                success:true,
+                message:"Guncelleme Basarili"
+            })
+        }
+        else return res.status(400).json({
+            success:false,
+            message:"Güncelleme Islemi Basarisiz Oldu"
+        })
+
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message:"Kayit Güncellenemedi"
+        })
+
+    }
+
+}
 
 module.exports={
-    todoAdd
+    todoAdd,
+    todoGetAll,
+    todoUpdate
 }
--- src/Routers/todoRouter.js
+++ src/Routers/todoRouter.js
@@ -1,4 +1,10 @@
 const router= require("express").Router()
 const todoController = require("../Controllers/todoController")
+
 router.post("/todo",todoController.todoAdd)
+
+router.get("/todo",todoController.todoGetAll)
+
+router.put("/todo/:id",todoController.todoUpdate)
+
 module.exports=router
