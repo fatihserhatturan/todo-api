0493dfa23891bb6cf51cedacfe28cd7ca5d8a12d
fatihserhatturan <95492309+fatihserhatturan@users.noreply.github.com>
2023-08-17 20:59:01 +0300

    api request

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express =require("express")
 const res = require("express/lib/response")
 const app = express()
+const cors = require("cors");
 require("dotenv").config()
 require("./src/config/databaseConnection")
 const port= process.env.PORT || 5001
 const todoRouter=require("./src/Routers/todoRouter")
 
 app.use(express.json())
+
+app.use(cors());
 
 app.use("/api",todoRouter)
 
